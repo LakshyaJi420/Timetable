5bde2fdcba10d2d676156def22769fe95977936d
LakshyaJi420 <160212507+LakshyaJi420@users.noreply.github.com>
2026-08-16 13:00:12 -0700

    Update build.gradle.kts

--- llama.android/lib/build.gradle.kts
+++ llama.android/lib/build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
     namespace = "com.arm.aichat"
+
     compileSdk = 36
 
     ndkVersion = "29.0.13113456"
@@ -12,54 +13,93 @@ android {
     defaultConfig {
         minSdk = 33
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
+        testInstrumentationRunner =
+            "androidx.test.runner.AndroidJUnitRunner"
+
+        consumerProguardFiles(
+            "consumer-rules.pro"
+        )
 
         ndk {
-             abiFilters += listOf("arm64-v8a", "x86_64")
+            abiFilters += listOf(
+                "arm64-v8a",
+                "x86_64"
+            )
         }
+
         externalNativeBuild {
             cmake {
-                arguments += "-DCMAKE_BUILD_TYPE=Release"
-                arguments += "-DCMAKE_MESSAGE_LOG_LEVEL=DEBUG"
-                arguments += "-DCMAKE_VERBOSE_MAKEFILE=ON"
+                arguments +=
+                    "-DCMAKE_BUILD_TYPE=Release"
 
-                arguments += "-DBUILD_SHARED_LIBS=ON"
-                arguments += "-DLLAMA_BUILD_APP=OFF"
-                arguments += "-DLLAMA_BUILD_COMMON=ON"
-                arguments += "-DLLAMA_OPENSSL=OFF"
+                arguments +=
+                    "-DCMAKE_MESSAGE_LOG_LEVEL=DEBUG"
 
-                arguments += "-DGGML_NATIVE=OFF"
-                arguments += "-DGGML_BACKEND_DL=ON"
-                arguments += "-DGGML_CPU_ALL_VARIANTS=ON"
-                arguments += "-DGGML_LLAMAFILE=OFF"
+                arguments +=
+                    "-DCMAKE_VERBOSE_MAKEFILE=ON"
+
+                arguments +=
+                    "-DBUILD_SHARED_LIBS=ON"
+
+                arguments +=
+                    "-DLLAMA_BUILD_APP=OFF"
+
+                arguments +=
+                    "-DLLAMA_BUILD_COMMON=ON"
+
+                arguments +=
+                    "-DLLAMA_OPENSSL=OFF"
+
+                arguments +=
+                    "-DGGML_NATIVE=OFF"
+
+                arguments +=
+                    "-DGGML_BACKEND_DL=ON"
+
+                arguments +=
+                    "-DGGML_CPU_ALL_VARIANTS=ON"
+
+                arguments +=
+                    "-DGGML_LLAMAFILE=OFF"
             }
         }
+
         aarMetadata {
             minCompileSdk = 35
         }
     }
+
     externalNativeBuild {
         cmake {
-            path("src/main/cpp/CMakeLists.txt")
+            path(
+                "src/main/cpp/CMakeLists.txt"
+            )
+
             version = "3.31.6"
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility =
+            JavaVersion.VERSION_17
+
+        targetCompatibility =
+            JavaVersion.VERSION_17
     }
+
     kotlin {
         jvmToolchain(17)
 
         compileOptions {
-            targetCompatibility = JavaVersion.VERSION_17
+            targetCompatibility =
+                JavaVersion.VERSION_17
         }
     }
 
     packaging {
         resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes +=
+                "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
 
@@ -71,9 +111,20 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.datastore.preferences)
 
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
+    implementation(
+        "androidx.core:core-ktx:1.17.0"
+    )
+
+    implementation(
+        "androidx.datastore:datastore-preferences:1.2.0"
+    )
+
+    testImplementation(
+        "junit:junit:4.13.2"
+    )
+
+    androidTestImplementation(
+        "androidx.test.ext:junit:1.3.0"
+    )
 }
